d9641d6dea519f7d543250f6cffb8cb95fd2fb47
Anna Dybas <a.m.dybas@gmail.com>
2019-11-21 05:48:23 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,3 +15,38 @@ def find_min_value(array)
   sorted_min_array = array.sort
   sorted_min_array[0]
 end
+
+=begin
+def find_element_index(array, value_to_find)
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+    end
+  end
+  nil
+end
+
+def find_max_value(array)
+  max = 0
+    array.length.times do |count|
+      if array[count] > max
+        max = array[count]
+      end
+    end
+  max
+end
+
+def find_min_value(array)
+  min = 0
+    array.length.times do |count|
+      if count == 0
+        min = array[count]
+      else
+        if array[count] < min
+          min = array[count]
+        end
+      end
+    end
+  min
+end
+=end
